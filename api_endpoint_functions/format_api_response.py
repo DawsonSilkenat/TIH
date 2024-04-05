95e56b41818e1cfa41dd2e19da5a5abcaac1cc2c
Dawson <dawsonsilkenat@gmail.com>
2024-04-05 12:43:23 +0100

    Final commit before leave

---
+++ api_endpoint_functions/format_api_response.py
@@ -0,0 +1,40 @@
+import re
+
+def format_api_response(api_response: dict[str, any]) -> str:
+    """Transforms the dictionary returned by the api into a string which can be processed by the llm
+
+    Args:
+        api_response (dict[str, any]): The list of results from the API
+
+    Returns:
+        str: A string formatted to be interpretted by the llm
+    """
+    
+    # For now we are only considering the name, description, and body for each response. 
+    name = api_response["name"]
+    description = api_response["description"]
+    body = api_response["body"]
+    
+    # Some responses contain newlines to format more nicely for a human reader. Remove these as it may lead to confusion to a machine reader, especially since we are using newlines to distinguish between name, description and body.
+    name = name.replace("\n", " ")
+    description = description.replace("\n", " ")
+    body = body.replace("\n", " ")
+    
+    # Some responses contain html, which doesn't add anything to the text and makes the result more difficult to read for debugging
+    regex = r"<.*?>"
+    
+    name = re.sub(regex, "", name)
+    description = re.sub(regex, "", description)
+    body = re.sub(regex, "", body)
+    
+    
+    # Regular expression to match any character outside the ASCII range
+    # We remove such characters as they are unlikely to appear frequently, if at all, in llm training
+    # TODO consider how I can exclude currency codes from this
+    regex = r"[^\x00-\x7F]"
+    
+    name = re.sub(regex, "", name)
+    description = re.sub(regex, "", description)
+    body = re.sub(regex, "", body)
+    
+    return f"{name}\n{description}\n{body}"
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from api_endpoint_functions.get_dataset_names import get_datasets
 from keywords import get_query_keywords
 from api_endpoint_functions.query_multiple_datasets import multiple_datasets_by_keywords
-from generate_reply import answer_user_query
+from api_endpoint_functions.format_api_response import format_api_response
+
+# from possible_actions.decide_action import decide_action
+from possible_actions.provide_answer import answer_user_query
+
 from flask import Flask, render_template, request
 import json
 
@@ -14,6 +18,7 @@ with open("api_keys.json", "r") as file:
 # For now I am just going to store the conversation. This should be replaced eventually, but good enough for poc
 conversation = []
 
+
 @app.route("/", methods=["GET", "POST"])
 def handle_query():
     llm_answer = None
@@ -22,8 +27,6 @@ def handle_query():
     if request.method == "POST":
         user_query = request.form["user_input"]
         conversation.append({"role": "user", "content": user_query})
-
-
         
         datasets = get_datasets(conversation, tih_api_key)
         print(datasets)
@@ -35,17 +38,20 @@ def handle_query():
         # This is the simplest method. I would also like to look at doing each endpoint individually, but not MVP 
         # Concern: Even though this may pull from multiple datasets, I don't think there is any reason to expect it will since the first n results may be from a single dataset
         api_responses = multiple_datasets_by_keywords(conversation, datasets, tih_api_key, keywords, 25)
+        api_responses = [format_api_response(response) for response in api_responses]
+        
+        for response in api_responses:
+            print(response)
+            print()
     
-        # Here would be the place to look at deals, augment the information provided to the llm
-    
-        # Once we have the api responce we need the llm to process the response 
+        
+        # llm_answer = decide_action(conversation, api_responses)
         llm_answer = answer_user_query(conversation, api_responses)
         
-        # chatgpt thinks it is writing markdown, so sometimes uses ** to make words bold. This doesn't work, either because it isn't valid html or because flask escapes it
-        llm_answer = llm_answer.replace("**", "") 
-        
-        
-        
+        # Here would be the place to look at deals, augment the information provided to the llm
+    
+        # # Once we have the api responce we need the llm to process the response 
+        # llm_answer = answer_user_query(conversation, api_responses)
         
         conversation.append({"role": "assistant", "content": llm_answer})
         
